17f8a65a7e230fea2127e64f42200f93df13c082
Kalyoncuguller <118815433+Kalyoncuguller@users.noreply.github.com>
2022-11-29 15:17:48 +0300

    güller
    exercise3

--- Exercises/3.exercise_simple.c
+++ Exercises/3.exercise_simple.c
@@ -1,2 +1,12 @@
 // Take a single character  as input, you may benefit from scanf("%c", &ch ); and print("%c", ch) to print the character. 
 // Write a program to print the ASCII value of the given character.
+
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    char ch;
+    printf("Bir karakter giriniz: ");
+    scanf("%c",&ch);
+    printf("Girilen degerin ASCII karsiligi %c = %d",ch,ch);
+    return 0;
+}
