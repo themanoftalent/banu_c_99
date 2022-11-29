230b08484b0ada09b76da47f8b5d9b0894712f40
Kalyoncuguller <118815433+Kalyoncuguller@users.noreply.github.com>
2022-11-29 14:43:43 +0300

    Güller
    3 yerine 5 kullanılacak

--- 9.pointer_ex.c
+++ 9.pointer_ex.c
@@ -5,7 +5,7 @@
 #include "string.h"
 
 int main() {
-    int numbers[3] = {1, 2, 3, 4, 5};
+    int numbers[5] = {1, 2, 3, 4, 5};
     int *ptr = numbers;
     for (int i = 0; i < 5; i++) {
         printf("%d ", *(ptr + i));
